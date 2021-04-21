ca686178049c41608100604d3ffc272ea09a2566
corey-power <CPower1248@gmail.com>
2021-04-21 15:55:15 -0400

    ...

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -20,6 +20,26 @@ export const initialState = {
     hereFromReducer: ""
 }
 
+const requests = {
+    satList: {
+        method: 'GET',
+        url: 'https://uphere-space1.p.rapidapi.com/satellite/list',
+        params: {page: '1'},
+        headers: {
+          'x-rapidapi-key': API_KEY,
+          'x-rapidapi-host': 'uphere-space1.p.rapidapi.com'
+        }
+    },
+    satByNumber: {
+        method: 'GET',
+        url: `https://uphere-space1.p.rapidapi.com/satellite/25544/details`,
+        headers: {
+          'x-rapidapi-key': API_KEY,
+          'x-rapidapi-host': 'uphere-space1.p.rapidapi.com'
+        }
+    }
+}
+
 export const reducer = (state = initialState, action) => {
     switch(action.type) { // [Optimize] Separate reducers
         // initial_a
